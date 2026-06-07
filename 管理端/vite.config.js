1f04e4be3391c66ae779cae1ab739b9c91bf0e22
Loveloli12 <1421392140@qq.com>
2026-06-07 16:29:13 +0800

    Add files via upload

---
+++ 管理端/vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    })
+  ],
+  server: {
+    host: "0.0.0.0",
+    proxy: {
+      "/v3pz": {
+        target: "https://v3pz.itndedu.com",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
+})
